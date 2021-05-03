e1edf6dec0c9660cfbf32d089ae9df2bcaa25c9f
markwilland <markwillanderson@gmail.com>
2021-05-02 22:43:29 -0400

    refactored reducer with memoization

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { fetchUser } from '../actions';
 
@@ -8,7 +8,7 @@ class UserHeader extends React.Component {
     }
 
     render() {
-        const user = this.props.users.find((user) => user.id === this.props.userId);
+        const { user } = this.props;
 
         if (!user) {
             return null;
@@ -18,8 +18,8 @@ class UserHeader extends React.Component {
     }
 }
 
-const mapStateToProps = (state) => {
-    return {users: state.users}
+const mapStateToProps = (state, ownProps) => {
+    return {user: state.users.find(user => user.id === ownProps.userId)}
 };
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
