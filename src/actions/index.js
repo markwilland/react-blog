1aa3fa5ee58c8397ca69bced009bdc1f7849c8e0
markwilland <markwillanderson@gmail.com>
2021-05-03 21:18:09 -0400

    fixed spacing removed comment

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,9 +12,9 @@ export const fetchPostsAndUsers = () => async (dispatch, getState) => {
 }
 
 export const fetchPosts = () => async dispatch => {
-        const response = await jsonPlaceholder.get('/posts')
+    const response = await jsonPlaceholder.get('/posts')
 
-        dispatch({type: 'FETCH_POSTS', payload: response.data});
+    dispatch({type: 'FETCH_POSTS', payload: response.data});
 };
 
 export const fetchUser =  id => async dispatch => {
@@ -22,11 +22,3 @@ export const fetchUser =  id => async dispatch => {
 
     dispatch({ type: 'FETCH_USER', payload: response.data })
 }
-
-// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-
-// const _fetchUser = _.memoize(async (id, dispatch) => {
-//     const response = await jsonPlaceholder.get(`/users/${id}`);
-
-//     dispatch({ type: 'FETCH_USER', payload: response.data })
-// })
